c7fc3b04111df4669b62bd847d8536b88ad54545
khamza Dzeytov <khamzadzeit00@gmail.com>
2023-10-02 12:05:22 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll', e => {
+	document.documentElement.style.setProperty('--scrollTop', `${this.scrollY}px`) // Update method
+})
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+ScrollSmoother.create({
+	wrapper: '.wrapper',
+	content: '.content'
+})
